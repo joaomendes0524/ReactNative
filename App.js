0c5857eae0ce7098a92ba7bf9f1978bbaa4e3373
unknown <JOO_23895@ETEC.FURLAN>
2023-08-14 21:31:41 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+function TextButton() {
+  return <Text>Olá Mundo </Text>;
+  }
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <TextButton />
+      <TextButton />
+      <TextButton />
     </View>
   );
 }
